5174aa46dc5e9c336c36730bb5e04bb1a835eafc
Roman Chistokhodov <freeslave93@gmail.com>
2022-09-18 03:25:54 +0300

    Add code for particle man implementation testing

--- cl_dll/tri.cpp
+++ cl_dll/tri.cpp
@@ -20,6 +20,7 @@
 
 #include "particleman.h"
 #include "tri.h"
+#include "com_model.h"
 extern IParticleMan* g_pParticleMan;
 
 /*
@@ -51,4 +52,38 @@ void DLLEXPORT HUD_DrawTransparentTriangles()
 
 	if (g_pParticleMan)
 		g_pParticleMan->Update();
+
+	extern Vector v_origin, v_angles;
+
+	static model_t* texture = 0;
+	static float nextParticle = 0;
+
+	if ( g_pParticleMan )
+	{
+		const float clTime = gEngfuncs.GetClientTime();
+
+		if (texture == 0)
+		{
+			texture = (model_t*)gEngfuncs.GetSpritePointer(SPR_Load("sprites/hotglow.spr"));
+		}
+
+		if (texture && nextParticle <= clTime)
+		{
+			Vector forward, right, up;
+			AngleVectors(v_angles, forward, right, up);
+
+			const Vector particleOrigin = v_origin + forward * 64.0f + up * gEngfuncs.pfnRandomFloat(-1.0f, 1.0f) * 32.0f;
+			CBaseParticle *particle = g_pParticleMan->CreateParticle(particleOrigin, Vector(0.0f, 0.0f, 0.0f), texture, 32.0f, 255.0f, "particle");
+
+			particle->SetLightFlag(LIGHT_NONE);
+			particle->SetCullFlag(CULL_PVS);
+			particle->SetRenderFlag(RENDER_FACEPLAYER);
+
+			particle->m_iRendermode = kRenderTransAdd;
+			particle->m_vColor = Vector(255, 255, 255);
+
+			particle->m_flDieTime = clTime + 1.0f;
+			nextParticle = clTime + 0.2f;
+		}
+	}
 }
